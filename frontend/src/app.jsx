afac0e500f08da37c6107e5750e2b9a697a77c7c
Prakhar Singh <prakkhar15@gmail.com>
2025-02-17 19:50:25 +0530

    added header component

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar'
 function App() {
 
   return (
-    <div className="mx-4 sm:mx-[10%]">
+    <div className="mx-4 sm:mx-[0.1%]">
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
