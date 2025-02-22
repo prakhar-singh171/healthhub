544d95ec56b1692ee8cf1a2a81cfb73c02f055a0
Prakhar Singh <prakkhar15@gmail.com>
2025-02-22 10:47:56 +0530

    initialized backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('API IS WORKING')
+})
+
+app.listen(port,()=>{
+    console.log("server is listing on",port);
+    
+})
