918208b166d94f3c206636968f9301a87bedd634
Prakhar Singh <prakkhar15@gmail.com>
2025-02-23 12:20:14 +0530

    adding login functionality for the admin and using JSON web token for secure login

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
-import { addDoctor } from '../controllers/doctorController.js'
+
+import { addDoctor,loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add-doctors',upload.single('image'),addDoctor)
+adminRouter.post('/login',loginAdmin);
+
 
 export default adminRouter;
