8c121b29d15955da4ea8ffcd0863b1d3c83de91a
Prakhar Singh <prakkhar15@gmail.com>
2025-02-24 19:04:38 +0530

    created authAdmin middleware

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -48,9 +48,10 @@ const addDoctor = async(req,res)=>{
 
 const loginAdmin = async (req,res)=>{
     try {
+
         const {email,password} = req.body;
         if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
-            const token =  jwt.sign(email+password,process.env.JWT_TOKEN)
+            const token =  jwt.sign(email+password,process.env.JWT_SECRET)
             res.json({success:true,token})
         }else{
             res.json({success:false,message:"Invalid Credatinals"})
