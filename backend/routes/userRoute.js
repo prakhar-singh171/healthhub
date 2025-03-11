021a4b210d586cc091691b4a339925331dedb401
Prakhar Singh <prakkhar15@gmail.com>
2025-03-11 14:40:50 +0530

    created userLogin api for the user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { registeruser } from '../controllers/userController.js';
+import { registeruser,userLogin } from '../controllers/userController.js';
 
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registeruser)
+userRouter.post('/login',userLogin)
 
 export default userRouter
