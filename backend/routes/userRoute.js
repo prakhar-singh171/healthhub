cd6c73d142f3bc163844d38f41e59741635ca285
Prakhar Singh <prakkhar15@gmail.com>
2025-03-11 19:23:18 +0530

    implementing updateProfile functionality with fileHandling

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { registeruser,userLogin,getProfile } from '../controllers/userController.js';
+import { registeruser,userLogin,getProfile,updateProfile } from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
+import upload from '../middlewares/multer.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registeruser)
 userRouter.post('/login',userLogin)
 userRouter.get('/get-profile',authUser,getProfile)
+userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 export default userRouter
