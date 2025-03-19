aa54566936b93d011fa3c998ef4e7a1f47908acf
Prakhar Singh <prakkhar15@gmail.com>
2025-03-19 23:12:55 +0530

    minor changes

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -3,13 +3,13 @@ import catchAsync from '../utils/catchAsync.js';
 const authAdmin = catchAsync(async (req, res, next) => {
         const authHeader = req.headers.authorization || req.headers.Authorization; 
         if (!authHeader) {
-            return res.json({ success: false, message: "Not Authorized, Login Again" });
+            return res.status(400).json({ success: false, message: "Not Authorized, Login Again" });
         }
         const token = authHeader.split(' ')[1]; 
         const decoded = jwt.verify(token, process.env.JWT_SECRET); 
 
         if (decoded.email !== process.env.ADMIN_EMAIL) {
-            return res.json({ success: false, message: "Not Authorized, Login Again" });
+            return res.status(400).json({ success: false, message: "Not Authorized, Login Again" });
         }
         req.user = decoded; 
         next();
