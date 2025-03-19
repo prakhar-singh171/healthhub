50ac34fc66496e738c92557444197cb23a853d16
Prakhar Singh <prakkhar15@gmail.com>
2025-03-19 22:37:05 +0530

    deploy issue

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -91,7 +91,7 @@ const AddDoctor = () => {
           </div>
           <div className="flex-1 flex-col gap-1 flex">
             <p>Experience</p>
-            <select onChange={(e)=>setExperience(e.target.value)} value={experience} className="border rounded px-3 py-2" name="" id="">
+            <select onChange={(e)=>setExperience(e.target.value)} value={experience} className="border rounded px-3 py-2" >
               <option value="1 Year">1 Year</option>
               <option value="2 Year">2 Year</option>
               <option value="3 Year">3 Year</option>
@@ -113,7 +113,7 @@ const AddDoctor = () => {
           <div className="w-full lg:flex-1 flex flex-col gap-4">
           <div className="flex-1 flex-col gap-1 flex">
             <p>Speciality</p>
-            <select onChange={(e)=>setSpeciality(e.target.value)} value={speciality} className="border rounded px-3 py-2" name="" id="">
+            <select onChange={(e)=>setSpeciality(e.target.value)} value={speciality} className="border rounded px-3 py-2">
               <option value="General physician">General physician</option>
               <option value="Gynecologist">Gynecologist</option>
               <option value="Dermatologist">Dermatologist</option>
