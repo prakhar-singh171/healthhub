9f0e623605e59da19905104fb68b32c15f82917f
Prakhar Singh <prakkhar15@gmail.com>
2025-03-19 18:40:53 +0530

    deploy issue

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,13 +45,12 @@ export const registeruser = catchAsync(async (req, res) => {
       const user = await newuser.save();
   
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-
       res.cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'None'
-      
-    });
+        httpOnly: true, 
+        secure: false, 
+        sameSite: 'None', 
+        maxAge: 7 * 24 * 60 * 60 * 1000, 
+      });
 
       res
         .status(200)
@@ -88,7 +87,7 @@ export const registeruser = catchAsync(async (req, res) => {
       res.cookie('token', token, {
         httpOnly: true, 
         secure: false, 
-        sameSite: 'Lax', 
+        sameSite: 'None', 
         maxAge: 7 * 24 * 60 * 60 * 1000, 
       });
   
