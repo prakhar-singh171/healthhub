7786db995a159d6a137b0a63eccb5dd257958c6e
Prakhar Singh <prakkhar15@gmail.com>
2025-03-19 20:58:50 +0530

    deploy fix

--- frontend/src/pages/VerifyEmail.jsx
+++ frontend/src/pages/VerifyEmail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
